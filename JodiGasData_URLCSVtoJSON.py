7ada66be75630d390c2547e27fea0d1fd29f12ad
Henrique <hvlino007@gmail.com>
2022-09-10 23:46:57 +0100

    first commit

--- JodiGasData_URLCSVtoJSON.py
+++ JodiGasData_URLCSVtoJSON.py
@@ -1,48 +1,45 @@
 import csv
-import json
 import urllib.request
 import zipfile
 from pathlib import Path
+import json
 
+with urllib.request.urlopen('https://www.jodidata.org/_resources/files/downloads/gas-data/jodi_gas_csv_beta.zip') as f:
+    with open(f'jodi_gas_csv_beta.zip', 'wb') as out_file:
+        out_file.write(f.read())
+with zipfile.ZipFile(f'jodi_gas_csv_beta.zip', 'r') as zip_ref:
+    zip_ref.extractall(Path(__file__).parent)
 
-def download_url(url, save_path):
-    with urllib.request.urlopen(url) as dl_file:
-        with open(save_path, 'wb') as out_file:
-            out_file.write(dl_file.read())
-
-
-dir_to_download_to = Path(__file__).parent
-
-download_url("https://www.jodidata.org/_resources/files/downloads/gas-data/jodi_gas_csv_beta.zip", f"{dir_to_download_to}/example.zip")
-
-with zipfile.ZipFile(f"{dir_to_download_to}/example.zip", 'r') as zip_ref:
-    zip_ref.extractall(dir_to_download_to)
-
-with open(f"{dir_to_download_to}/jodi_gas_beta.csv", 'r') as file:
+with open(f'jodi_gas_beta.csv', 'r') as file:
     reader = csv.DictReader(file)
-    next(reader)
-    data = {"series_id": [], "points": [], "fields": {"country": "", "concept": "", "units": [], "source": ""}}
-    countriesList = []
-    datesList = []
+    dataList = []
 
-    for i, row in enumerate(reader):
-        if row["REF_AREA"] == 'BR':
-            datesList.append(row["TIME_PERIOD"])
-            for j, el in enumerate(datesList):
-                key = "series_id"
-                data.setdefault(key, [])
-                [data["series_id"].append(row['REF_AREA']) for i in [row['REF_AREA']] if i not in data[key]]
+    for row in reader:
+        currentId = [row['REF_AREA'], row['ENERGY_PRODUCT'], row['FLOW_BREAKDOWN'], row['UNIT_MEASURE']]
+        timeObsValue = [row['TIME_PERIOD'] + '-01', row['OBS_VALUE']]
+        if not any(d['series_id'] == currentId for d in dataList):
+            data = {
+                'series_id': currentId,
+                'points': [timeObsValue],
+                'fields': {
+                    'REF_AREA': row['REF_AREA'],
+                    'ENERGY_PRODUCT': row['ENERGY_PRODUCT'],
+                    'FLOW_BREAKDOWN': row['FLOW_BREAKDOWN'],
+                    'UNIT_MEASURE': row['UNIT_MEASURE'],
+                    'ASSESSMENT_CODE': row['ASSESSMENT_CODE'],
+                    'SOURCE': 'https://www.jodidata.org/_resources/files/downloads/gas-data/jodi_gas_csv_beta.zip'
+                }
+            }
+            dataList.append(data)
+        else:
+            for dic in dataList:
+                if dic['series_id'] == currentId:
+                    dic['points'].append(timeObsValue)
+                    dic['points'] = dic['points'][-5:]
 
-                key = "points"
-                if [datesList[j]] not in data[key]:
-                    data[key].append([datesList[j]])
-
-                key = "fields"
-                data[key]['country'] = ([row["REF_AREA"]])
-                data[key]['concept'] = "Natural Gas"
-                if row['UNIT_MEASURE'] not in data[key]['units']:
-                    data[key]['units'].append(row['UNIT_MEASURE'])
-                data[key]['source'] = "The JODI Gas World Database"
-
-parsed = json.dumps(data, indent=4)
-print(parsed)
+for d in dataList:
+    d['series_id'] = '\\'.join(d['series_id'])
+finalJson = '[\n' + ',\n'.join(map(json.dumps, dataList)) + '\n]'
+with open('seriesOutput.json', 'w') as f:
+    f.write(finalJson)
+print(finalJson)
