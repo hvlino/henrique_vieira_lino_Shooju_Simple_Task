96f0aa7acbed4c57730376437d646fa47ffe5828
Henrique <hvlino007@gmail.com>
2022-09-11 03:35:39 +0100

    minor changes and formatting. Now extracting without zip

--- JodiGasData_URLCSVtoJSON.py
+++ JodiGasData_URLCSVtoJSON.py
@@ -1,26 +1,32 @@
 import csv
-import urllib.request
-import zipfile
-from pathlib import Path
 import json
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
 
-with urllib.request.urlopen('https://www.jodidata.org/_resources/files/downloads/gas-data/jodi_gas_csv_beta.zip') as f:
-    with open(f'jodi_gas_csv_beta.zip', 'wb') as out_file:
-        out_file.write(f.read())
-with zipfile.ZipFile(f'jodi_gas_csv_beta.zip', 'r') as zip_ref:
-    zip_ref.extractall(Path(__file__).parent)
+with urlopen('https://www.jodidata.org/_resources/files/downloads/gas-data/jodi_gas_csv_beta.zip') as zipresp:
+    with ZipFile(BytesIO(zipresp.read())) as zfile:
+        zfile.extractall()
 
 with open(f'jodi_gas_beta.csv', 'r') as file:
     reader = csv.DictReader(file)
-    dataList = []
+    list_of_dicts = []
 
     for row in reader:
-        currentId = [row['REF_AREA'], row['ENERGY_PRODUCT'], row['FLOW_BREAKDOWN'], row['UNIT_MEASURE']]
-        timeObsValue = [row['TIME_PERIOD'] + '-01', row['OBS_VALUE']]
-        if not any(d['series_id'] == currentId for d in dataList):
+        current_id = [
+            row['REF_AREA'],
+            row['ENERGY_PRODUCT'],
+            row['FLOW_BREAKDOWN'],
+            row['UNIT_MEASURE']
+        ]
+        current_time_obsvalue = [
+            row['TIME_PERIOD'] + '-01',
+            row['OBS_VALUE']
+        ]
+        if not any(d['series_id'] == current_id for d in list_of_dicts):
             data = {
-                'series_id': currentId,
-                'points': [timeObsValue],
+                'series_id': current_id,
+                'points': [current_time_obsvalue],
                 'fields': {
                     'REF_AREA': row['REF_AREA'],
                     'ENERGY_PRODUCT': row['ENERGY_PRODUCT'],
@@ -30,16 +36,19 @@ with open(f'jodi_gas_beta.csv', 'r') as file:
                     'SOURCE': 'https://www.jodidata.org/_resources/files/downloads/gas-data/jodi_gas_csv_beta.zip'
                 }
             }
-            dataList.append(data)
+            list_of_dicts.append(data)
         else:
-            for dic in dataList:
-                if dic['series_id'] == currentId:
-                    dic['points'].append(timeObsValue)
+            for dic in list_of_dicts:
+                if dic['series_id'] == current_id:
+                    dic['points'].append(current_time_obsvalue)
                     dic['points'] = dic['points'][-5:]
 
-for d in dataList:
-    d['series_id'] = '\\'.join(d['series_id'])
-finalJson = '[\n' + ',\n'.join(map(json.dumps, dataList)) + '\n]'
-with open('seriesOutput.json', 'w') as f:
-    f.write(finalJson)
-print(finalJson)
+for elem_data in list_of_dicts:
+    elem_data['series_id'] = '\\'.join(elem_data['series_id'])
+
+parsed = '[\n' + ',\n'.join(map(json.dumps, list_of_dicts)) + '\n]'
+
+with open('seriesOutput.json', 'w') as file_out:
+    file_out.write(parsed)
+
+print(parsed)
